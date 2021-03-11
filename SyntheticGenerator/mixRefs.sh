0222f162f8cfc40d6589bc424b2aa47525da5343
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-03-11 21:27:28 +0000

    Generator of Synthetic Sequences

---
+++ SyntheticGenerator/mixRefs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# ===================================================== #
+# =                                                   = #
+# =                  References Mixer                 = #
+# =                                                   = #
+# ===================================================== #
+# =                                                   = #
+# =   Usage: sh mixRefs.sh [FILE1] [FILE2] : [FILE9]  = #
+# =                                                   = #
+# ===================================================== #
+
+# VARIABLES
+NUM_FILES=0;
+FILE1=$1;
+FILE2=$2;
+FILE3=$3;
+FILE4=$4;
+FILE5=$5;
+FILE6=$6;
+FILE7=$7;
+FILE8=$8;
+FILE9=$9;
+
+if [ -n "$FILE1" ]; then
+        echo "Start File Concat..."
+else
+	echo "Argument Error - Insert Reference(s) File(s)"
+        echo "Usage: bash mixRefs.sh [FILE1] [FILE2] : [FILE9]"
+	exit 0
+fi
+
+NUM_FILES=$( ls SyntheticGenerator/Inputs/ | wc -l )
+NUM_FILES=$((NUM_FILES+1))
+
+cat $1 $2 $3 $4 $5 $6 $7 $8 $9 > "SyntheticGenerator/Inputs/mix${NUM_FILES}.fa"
+echo "Success! File mix${NUM_FILES}.fa has been created."
+
+#echo "${NUM_FILES}"
---
+++ SyntheticGenerator/syntheticGen.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# ============================================================== #
+# =                                                            = #
+# =                     Synthetic Generator                    = #
+# =                                                            = #
+# ============================================================== #
+# =                                                            = #
+# =                Usage: sh syntheticGen.sh [FILE]            = #
+# =                                                            = #
+# ============================================================== #
+#
+# ART ILLUMINA VALUES
+RAND_SEED=0;				# -rs
+SEQ_SYSTEM="HS25";			# -ss
+ART_INPUT=$1;				# -i
+ART_LEN=150;				# -l
+ART_FOLD=40;				# -f
+ART_SIZE=200;				# -m
+ART_DSV=10;				# -s
+ART_OUTPUT="SyntheticData/sample";	# -o
+
+# GTO GENERATOR VALUES
+GTO_NUM_SYMBOLS=5000;			# -n
+GTO_LINE_SIZE=70;			# -l
+
+
+if [ -n "$ART_INPUT" ]; then
+	echo "Start FASTA Processing..."
+else
+	echo "Argument Error - Insert Input File"
+	echo "Usage: bash syntheticGen.sh [FILE]"
+	exit 0
+fi
+
+# CLEAR OLD GENERATED FILES
+rm SyntheticData/*
+#rm -rf SyntheticOutputs/out_spades_
+
+# GENERATE RANDOM DNA GENOME
+gto_genomic_gen_random_dna -n $GTO_NUM_SYMBOLS | gto_fasta_from_seq -n GTO_Genomic_Generator_Random_DNA_Sequence -l $GTO_LINE_SIZE > newSeq.fa
+
+cat $ART_INPUT newSeq.fa > tmp.fa
+echo "" >> tmp.fa
+rm newSeq.fa $ART_INPUT
+mv tmp.fa $ART_INPUT
+
+# GENERATE SYNTHETIC SEQUENCE
+art_illumina -rs $RAND_SEED -ss $SEQ_SYSTEM -sam -i $ART_INPUT -p -l $ART_LEN -f $ART_FOLD -m $ART_SIZE -s $ART_DSV -o $ART_OUTPUT
+
+# GZIP FASTQ FILES
+gzip SyntheticData/sample*.fq
