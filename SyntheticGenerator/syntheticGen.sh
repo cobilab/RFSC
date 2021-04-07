7a40e1bc638871669314f9fa13c97f2d6a873bab
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-04-07 19:25:38 +0100

    Modified input folder

--- SyntheticGenerator/syntheticGen.sh
+++ SyntheticGenerator/syntheticGen.sh
@@ -18,7 +18,7 @@ ART_LEN=150;						# -l
 ART_FOLD=40;						# -f
 ART_SIZE=200;						# -m
 ART_DSV=10;							# -s
-ART_OUTPUT="SyntheticData/sample";	# -o
+ART_OUTPUT="Input_Data/sample";	# -o
 
 # GTO GENERATOR VALUES
 GTO_NUM_SYMBOLS=5000;				# -n
@@ -34,8 +34,7 @@ else
 fi
 
 # CLEAR OLD GENERATED FILES
-rm SyntheticData/sample*
-#rm -rf SyntheticOutputs/out_spades_
+rm Input_Data/sample*
 
 # GENERATE RANDOM DNA GENOME
 gto_genomic_gen_random_dna -n $GTO_NUM_SYMBOLS | gto_fasta_from_seq -n GTO_Genomic_Generator_Random_DNA_Sequence -l $GTO_LINE_SIZE > newSeq.fa
@@ -49,4 +48,4 @@ mv tmp.fa $ART_INPUT
 art_illumina -rs $RAND_SEED -ss $SEQ_SYSTEM -sam -i $ART_INPUT -p -l $ART_LEN -f $ART_FOLD -m $ART_SIZE -s $ART_DSV -o $ART_OUTPUT
 
 # GZIP FASTQ FILES
-gzip SyntheticData/sample*.fq
+gzip Input_Data/sample*.fq
