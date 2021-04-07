552611ff15b3b05d4d5252e0748dc5f73a412970
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-04-07 22:18:25 +0100

    Script for building reference virus database

---
+++ src/build_ref_db_virus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd References/NCBI-Virus/
+
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/viral/assembly_summary.txt
+awk -F '\t' '{if($12=="Complete Genome") print $20}' assembly_summary.txt > ASCG.txt
+mkdir -p GB_DB_VIRAL
+mkdir -p GB_DB_VIRAL_CDS
+mkdir -p GB_DB_VIRAL_RNA
+cat ASCG.txt | xargs -I{} -n1 -P8 wget -P GB_DB_VIRAL {}/*_genomic.fna.gz
+mv GB_DB_VIRAL/*_cds_from_genomic.fna.gz GB_DB_VIRAL_CDS/
+mv GB_DB_VIRAL/*_rna_from_genomic.fna.gz GB_DB_VIRAL_RNA/
+zcat GB_DB_VIRAL/*.fna.gz > VDB.fa
