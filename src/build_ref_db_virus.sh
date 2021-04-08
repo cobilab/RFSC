ba3b8e4f962c54e794ff98d16559580997207185
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-04-08 15:43:00 +0100

    Update script of reference virus db build

--- src/build_ref_db_virus.sh
+++ src/build_ref_db_virus.sh
@@ -13,3 +13,5 @@ cat ASCG.txt | xargs -I{} -n1 -P8 wget -P GB_DB_VIRAL {}/*_genomic.fna.gz
 mv GB_DB_VIRAL/*_cds_from_genomic.fna.gz GB_DB_VIRAL_CDS/
 mv GB_DB_VIRAL/*_rna_from_genomic.fna.gz GB_DB_VIRAL_RNA/
 zcat GB_DB_VIRAL/*.fna.gz > VDB.fa
+
+echo -e "\033[1;34m[NCRS] \033[1;32m Building has been successful! \033[0m";
