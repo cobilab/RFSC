d76030a877f5fba58ed8f8583f325d936838e535
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-04-30 12:10:58 +0100

    generalization of the method

--- src/dna_compression_analysis_csv.sh
+++ src/dna_compression_analysis_csv.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
+# run: ./src/dna_compression_analysis_csv.sh Virus VIRAL geco3_Viral.csv
+#      ./src/dna_compression_analysis_csv.sh Fungi FUNGI geco3_Fungi.csv
+
+
 declare -a array_identifier
 declare -a array_size
 declare -a array_level
 runs=0;
-#files=0;
 file_res=0;
 
-printf "Identifier\tSize1\tSize2\tSize3\tSize4\tSize7\tSize12\tSize14\tLevel1\tLevel2\tLevel3\tLevel4\tLevel7\tLevel12\tLevel14\n" >> GeCo3_Output/Virus/geco3_Viral.csv
+printf "Identifier\tSize1\tSize2\tSize3\tSize4\tSize7\tSize12\tSize14\tLevel1\tLevel2\tLevel3\tLevel4\tLevel7\tLevel12\tLevel14\n" >> GeCo3_Output/$1/$3
 
-readarray -t array_identifier < GeCo3_Output/Virus/VIRAL_ID.txt
-
-# for file in References/NCBI-Virus/*
-# do
-#     if [[ "$file" == "viral_url_donwload.txt" ]]; then
-#         echo -e "\033[1;34m[RFSC]\033[0m Jumping index file ($file)";
-#     else
-#         out_file=$(basename $file);
-#         array_identifier[$files]="${out_file%".fna.gz"}"
-#         (( files++ ))
-#     fi
-# done
+readarray -t array_identifier < GeCo3_Output/$1/$2_ID.txt
 
 while read line; 
 do
     if [[ "$runs" == "7" ]]; then
-        printf "${array_identifier[$file_res]}\t${array_size[0]}\t${array_size[1]}\t${array_size[2]}\t${array_size[3]}\t${array_size[4]}\t${array_size[5]}\t${array_size[6]}\t${array_level[0]}\t${array_level[1]}\t${array_level[2]}\t${array_level[3]}\t${array_level[4]}\t${array_level[5]}\t${array_level[6]}\n" >> GeCo3_Output/Virus/geco3_Viral.csv
+        printf "${array_identifier[$file_res]}\t${array_size[0]}\t${array_size[1]}\t${array_size[2]}\t${array_size[3]}\t${array_size[4]}\t${array_size[5]}\t${array_size[6]}\t${array_level[0]}\t${array_level[1]}\t${array_level[2]}\t${array_level[3]}\t${array_level[4]}\t${array_level[5]}\t${array_level[6]}\n" >> GeCo3_Output/$1/$3
         (( file_res++ ))
         runs=0;
         array_size=()
@@ -37,10 +29,7 @@ do
     array_level[$runs]=`echo "scale=5; ${array_level[$runs]}/2" | bc`
     (( runs++ ))
     
-done <GeCo3_Output/Virus/geco_VIRAL.txt
+done <GeCo3_Output/$1/geco_$2.txt
 
 # Input of the last set of data
-printf "${array_identifier[$file_res]}\t${array_size[0]}\t${array_size[1]}\t${array_size[2]}\t${array_size[3]}\t${array_size[4]}\t${array_size[5]}\t${array_size[6]}\t${array_level[0]}\t${array_level[1]}\t${array_level[2]}\t${array_level[3]}\t${array_level[4]}\t${array_level[5]}\t${array_level[6]}\n" >> GeCo3_Output/Virus/geco3_Viral.csv
-
-#echo "${array_size[0]}"
-#echo "${array_level[6]}"
+printf "${array_identifier[$file_res]}\t${array_size[0]}\t${array_size[1]}\t${array_size[2]}\t${array_size[3]}\t${array_size[4]}\t${array_size[5]}\t${array_size[6]}\t${array_level[0]}\t${array_level[1]}\t${array_level[2]}\t${array_level[3]}\t${array_level[4]}\t${array_level[5]}\t${array_level[6]}\n" >> GeCo3_Output/$1/$3
