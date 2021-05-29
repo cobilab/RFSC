28247ddae254041b7efb731c5403493e4f68c945
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-05-29 02:23:58 +0100

    Updated usage to level 7

--- src/naiveBayes.py
+++ src/naiveBayes.py
@@ -4,66 +4,151 @@ import matplotlib.pyplot as plt
 import csv
 from statistics import NormalDist
 
-virus_nm_size1, virus_nm_size2, virus_nm_size3, virus_nm_size4, virus_nm_size7, virus_nm_size12, virus_nm_size14 = [], [], [], [], [], [], []
-virus_nm_result1, virus_nm_result2, virus_nm_result3, virus_nm_result4, virus_nm_result7, virus_nm_result12, virus_nm_result14 = [], [], [], [], [], [], []
-virus_pt_size1, virus_pt_size2, virus_pt_size3, virus_pt_size4, virus_pt_size7, virus_pt_size12, virus_pt_size14 = [], [], [], [], [], [], []
-virus_pt_result1, virus_pt_result2, virus_pt_result3, virus_pt_result4, virus_pt_result7, virus_pt_result12, virus_pt_result14 = [], [], [], [], [], [], []
+# ( https://www.youtube.com/watch?v=rzFX5NWojp0 ) 
+
+## Virus Values
+virus_nm_result7, virus_pt_result7 = [], []
+
+## Bacterias Values
+bacteria_nm_result7, bacteria_pt_result7 = [], []
+
+## Archaeas Values
+archaea_nm_result7, archaea_pt_result7 = [], []
+
+## Fungis Values
+fungi_nm_result7, fungi_pt_result7 = [], []
+
+## Plants Values
+plant_nm_result7, plant_pt_result7 = [], []
+
+## Protozoas Values
+protozoa_nm_result7, protozoa_pt_result7 = [], []
+
+## Mitochondrials Values
+mito_nm_result7, mito_pt_result7 = [], []
+
+## Platids Values
+plastid_nm_result7, plastid_pt_result7 = [], []
 
 
+# Viral CSV files
 with open('GeCo3_Output/Virus/NM/geco3_Viral.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        virus_nm_size1.append(row[0].split("\t")[1])
-        virus_nm_size2.append(row[0].split("\t")[2])
-        virus_nm_size3.append(row[0].split("\t")[3])
-        virus_nm_size4.append(row[0].split("\t")[4])
-        virus_nm_size7.append(row[0].split("\t")[5])
-        virus_nm_size12.append(row[0].split("\t")[6])
-        virus_nm_size14.append(row[0].split("\t")[7])
-
-        virus_nm_result1.append("0"+row[0].split("\t")[8])
-        virus_nm_result2.append("0"+row[0].split("\t")[9])
-        virus_nm_result3.append("0"+row[0].split("\t")[10])
-        virus_nm_result4.append("0"+row[0].split("\t")[11])
         virus_nm_result7.append("0"+row[0].split("\t")[12])
-        virus_nm_result12.append("0"+row[0].split("\t")[13])
-        virus_nm_result14.append("0"+row[0].split("\t")[14])
 
 with open('GeCo3_Output/Virus/PT/geco3_Viral.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        virus_pt_size1.append(row[0].split("\t")[1])
-        virus_pt_size2.append(row[0].split("\t")[2])
-        virus_pt_size3.append(row[0].split("\t")[3])
-        virus_pt_size4.append(row[0].split("\t")[4])
-        virus_pt_size7.append(row[0].split("\t")[5])
-        virus_pt_size12.append(row[0].split("\t")[6])
-        virus_pt_size14.append(row[0].split("\t")[7])
-
-        virus_pt_result1.append("0"+row[0].split("\t")[8])
-        virus_pt_result2.append("0"+row[0].split("\t")[9])
-        virus_pt_result3.append("0"+row[0].split("\t")[10])
-        virus_pt_result4.append("0"+row[0].split("\t")[11])
         virus_pt_result7.append("0"+row[0].split("\t")[12])
-        virus_pt_result12.append("0"+row[0].split("\t")[13])
-        virus_pt_result14.append("0"+row[0].split("\t")[14])
+
+'''
+# Bacterias CSV files
+with open('GeCo3_Output/Bacteria/NM/geco3_Bacteria.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        bacteria_nm_result7.append("0"+row[0].split("\t")[12])
+
+with open('GeCo3_Output/Bacteria/PT/geco3_Bacteria.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        bacteria_pt_result7.append("0"+row[0].split("\t")[12])
+'''
+
+# Archaeas CSV files
+with open('GeCo3_Output/Archaea/NM/geco3_Archaea.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        archaea_nm_result7.append("0"+row[0].split("\t")[12])
+
+with open('GeCo3_Output/Archaea/PT/geco3_Archaea.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        archaea_pt_result7.append("0"+row[0].split("\t")[12])
+
+# Fungis CSV files
+with open('GeCo3_Output/Fungi/NM/geco3_Fungi.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        fungi_nm_result7.append("0"+row[0].split("\t")[12])
+
+with open('GeCo3_Output/Fungi/PT/geco3_Fungi.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        fungi_pt_result7.append("0"+row[0].split("\t")[12])
+
+# Plants CSV files
+with open('GeCo3_Output/Plant/NM/geco3_Plant.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        plant_nm_result7.append("0"+row[0].split("\t")[12])
+
+with open('GeCo3_Output/Plant/PT/geco3_Plant.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        plant_pt_result7.append("0"+row[0].split("\t")[12])
+
+# Protozoa CSV files
+with open('GeCo3_Output/Protozoa/NM/geco3_Protozoa.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        protozoa_nm_result7.append("0"+row[0].split("\t")[12])
+
+with open('GeCo3_Output/Protozoa/PT/geco3_Protozoa.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        protozoa_pt_result7.append("0"+row[0].split("\t")[12])
+
+# Mitochondrial CSV files
+with open('GeCo3_Output/Mitochondrial/NM/geco3_Mitochondrial.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        mito_nm_result7.append("0"+row[0].split("\t")[12])
+
+with open('GeCo3_Output/Mitochondrial/PT/geco3_Mitochondrial.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        mito_pt_result7.append("0"+row[0].split("\t")[12])
+
+# Plastid CSV files
+with open('GeCo3_Output/Plastid/NM/geco3_Plastid.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        plastid_nm_result7.append("0"+row[0].split("\t")[12])
+
+with open('GeCo3_Output/Plastid/PT/geco3_Plastid.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        plastid_pt_result7.append("0"+row[0].split("\t")[12])
+
+# /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ 
+# \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ 
+# /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ 
+# \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ 
 
 # Generate some data for this demonstration.
 #data = norm.rvs(10.0, 2.5, size=500)
 #data = [8, 12, 7, 6, 2, 6, 7, 4, 3, 2, 5]
-virus_nm_result1 = np.array(virus_nm_result1[1:], dtype=float)
-virus_nm_result2 = np.array(virus_nm_result2[1:], dtype=float)
-virus_nm_result3 = np.array(virus_nm_result3[1:], dtype=float)
-virus_nm_result4 = np.array(virus_nm_result4[1:], dtype=float)
 virus_nm_result7 = np.array(virus_nm_result7[1:], dtype=float)
-virus_nm_result12 = np.array(virus_nm_result12[1:], dtype=float)
-virus_nm_result14 = np.array(virus_nm_result14[1:], dtype=float)
+virus_pt_result7 = np.array(virus_pt_result7[1:], dtype=float)
+bacteria_nm_result7 = np.array(bacteria_nm_result7[1:], dtype=float)
+bacteria_pt_result7 = np.array(bacteria_pt_result7[1:], dtype=float)
+fungi_nm_result7 = np.array(fungi_nm_result7[1:], dtype=float)
+fungi_pt_result7 = np.array(fungi_pt_result7[1:], dtype=float)
+plant_nm_result7 = np.array(plant_nm_result7[1:], dtype=float)
+plant_pt_result7 = np.array(plant_pt_result7[1:], dtype=float)
+protozoa_nm_result7 = np.array(protozoa_nm_result7[1:], dtype=float)
+protozoa_pt_result7 = np.array(protozoa_pt_result7[1:], dtype=float)
+mito_nm_result7 = np.array(mito_nm_result7[1:], dtype=float)
+mito_pt_result7 = np.array(mito_pt_result7[1:], dtype=float)
+plastid_nm_result7 = np.array(plastid_nm_result7[1:], dtype=float)
+plastid_pt_result7 = np.array(plastid_pt_result7[1:], dtype=float)
 
 # Fit a normal distribution to the data:
-mu, std = norm.fit(virus_nm_result3)
+mu, std = norm.fit(virus_nm_result7)
 
 # Plot the histogram.
-plt.hist(virus_nm_result3, bins=25, density=True, alpha=0.6, color='g')
+plt.hist(virus_nm_result7, bins=25, density=True, alpha=0.6, color='g')
 
 # Plot the PDF.
 xmin, xmax = plt.xlim()
@@ -75,8 +160,8 @@ plt.title(title)
 
 plt.show()
 
-print(x)
-print("-----")
-print(p)
-print("-----")
-print(NormalDist(mu,std).pdf(0.9))
+#print(x)
+#print("-----")
+#print(p)
+#print("-----")
+print(NormalDist(mu,std).pdf(0.91))
