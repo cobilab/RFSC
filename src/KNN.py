01a8f92f9ab4f68d75e201bc9685a34a3eb34c92
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-07-15 10:27:05 +0100

    Fully operational with 5 predictors

--- src/KNN.py
+++ src/KNN.py
@@ -1,20 +1,24 @@
+'''
+	Usage: python3 src/KNN.py 2 0.7545 0.783272 0.19727258 1.927032e-10 3.87563e-11
+'''
+
 # Packages for analysis
 import pandas as pd
 import numpy as np
 from math import sqrt
 import csv, sys
 
-K = 3;
+K = int(sys.argv[1]);				# K-Neighbors
 
-dna_input = float(sys.argv[1])    # 
-aa_input = float(sys.argv[1])    # 
-gc_input = float(sys.argv[1])    # 
-
-#samples = pd.read_csv('Analysis/KNN/Domains.csv', header=0)
+dna_input = float(sys.argv[2])   	# DNA Compression Value
+aa_input = float(sys.argv[3])    	# AA Compression Value
+gc_content = float(sys.argv[4])    	# GC-Content Value
+len_dna_input = float(sys.argv[5])  # Length of the DNA sequence
+len_aa_input = float(sys.argv[6])   # Length of the AA sequence
 
 samplesDomains = ["Viral", "Bacteria", "Archaea", "Fungi", "Plant", "Protozoa", "Mitochondrial", "Plastid"]
-
 train_samples = []
+to_predict = [dna_input, aa_input, gc_content, len_dna_input, len_aa_input]
 
 # calculate the Euclidean distance between two vectors
 def euclidean_distance(row1, row2):
@@ -49,17 +53,15 @@ with open('Analysis/KNN/Domains.csv', 'r') as file:
     for row in samples:
         #if (row[0] == "Viral"):
             #train_samples.append([float(row[1]),float(row[2]),row[0]])
-        train_samples.append([float(row[1]),float(row[2]),row[0]])
+        train_samples.append([float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),row[0]])
             
-
-row0 = [dna_input, aa_input, gc_input]
 #for row in train_samples:
-#	distance = euclidean_distance(row0, row)
+#	distance = euclidean_distance(to_predict, row)
 #	print(distance)
 
-#neighbors = get_neighbors(train_samples, row0, K)
+#neighbors = get_neighbors(train_samples, to_predict, K)
 #for neighbor in neighbors:
 #	print(neighbor)
 
-prediction = predict_classification(train_samples, row0, K)
+prediction = predict_classification(train_samples, to_predict, K)
 print(prediction)
