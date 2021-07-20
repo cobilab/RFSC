72ed6aeb23ec4e02c3d177042ac0bb9b50394697
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-07-20 21:58:11 +0100

    Updated Cross-Validation method

--- src/KNN.py
+++ src/KNN.py
@@ -64,15 +64,47 @@ if Mode == "Test":
 			train_samples.append([float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),row[0]])
 
 elif Mode == "CV":
-	numberSamples=0
+	numberSamples = [0,0,0,0,0,0,0,0,0]
 	with open('Analysis/KNN/Train.csv', 'r') as file:
 		samples = csv.reader(file)
 		next(samples)
 		for row in samples:
-			if row[0] == CV_Domain:
-				numberSamples=numberSamples+1
-	numFiles_ForTestMin = int(numberSamples * crossValidation_Min_Lim)
-	numFiles_ForTestMax = int(numberSamples * crossValidation_Max_Lim)
+			if row[0] == "Viral":
+				numberSamples[0] = numberSamples[0] + 1;
+			elif row[0] == "Bacteria":
+				numberSamples[1] = numberSamples[1] + 1;
+			elif row[0] == "Archaea":
+				numberSamples[2] = numberSamples[2] + 1;
+			elif row[0] == "Fungi":
+				numberSamples[3] = numberSamples[3] + 1;
+			elif row[0] == "Plant":
+				numberSamples[4] = numberSamples[4] + 1;
+			elif row[0] == "Protozoa":
+				numberSamples[5] = numberSamples[5] + 1;
+			elif row[0] == "Mitochondrial":
+				numberSamples[6] = numberSamples[6] + 1;
+			elif row[0] == "Plastid":
+				numberSamples[7] = numberSamples[7] + 1;
+
+	if CV_Domain == "Viral":
+		limSamples = numberSamples[0]
+	elif CV_Domain == "Bacteria":
+		limSamples = numberSamples[0] + numberSamples[1]
+	elif CV_Domain == "Archaea":
+		limSamples = numberSamples[0] + numberSamples[1] + numberSamples[2]
+	elif CV_Domain == "Fungi":
+		limSamples = numberSamples[0] + numberSamples[1] + numberSamples[2] + numberSamples[3]
+	elif CV_Domain == "Plant":
+		limSamples = numberSamples[0] + numberSamples[1] + numberSamples[2] + numberSamples[3] + numberSamples[4]
+	elif CV_Domain == "Protozoa":
+		limSamples = numberSamples[0] + numberSamples[1] + numberSamples[2] + numberSamples[3] + numberSamples[4] + numberSamples[5]
+	elif CV_Domain == "Mitochondrial":
+		limSamples = numberSamples[0] + numberSamples[1] + numberSamples[2] + numberSamples[3] + numberSamples[4] + numberSamples[5] + numberSamples[6] 
+	elif CV_Domain == "Plastid":
+		limSamples = numberSamples[0] + numberSamples[1] + numberSamples[2] + numberSamples[3] + numberSamples[4] + numberSamples[5] + numberSamples[6] + numberSamples[7]
+
+	numFiles_ForTestMin = int(limSamples * crossValidation_Min_Lim)
+	numFiles_ForTestMax = int(limSamples * crossValidation_Max_Lim)
 
 	sampleCounter = 0;
 
@@ -80,17 +112,15 @@ elif Mode == "CV":
 		samples = csv.reader(file)
 		next(samples)
 		for row in samples:
-			if row[0] == CV_Domain:
-				if sampleCounter >= numFiles_ForTestMin and sampleCounter <= numFiles_ForTestMax:
-					sampleCounter = sampleCounter + 1
-				else:
-					train_samples.append([float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),row[0]])
-					sampleCounter = sampleCounter + 1
+			if sampleCounter >= numFiles_ForTestMin and sampleCounter <= numFiles_ForTestMax:
+				sampleCounter = sampleCounter + 1
+			else:
+				train_samples.append([float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),row[0]])
+				sampleCounter = sampleCounter + 1
 
 else:
 	print("Mode not found. Please specify the correct mode of use: Test or CV")
-
-            
+      
 #for row in train_samples:
 #	distance = euclidean_distance(to_predict, row)
 #	print(distance)
