e40de8749c47abe8456c6ab75c037220530d3667
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-07-21 10:36:01 +0100

    Updated Outputs

--- Tests/accuracy_CV.py
+++ Tests/accuracy_CV.py
@@ -1,5 +1,5 @@
 '''
-  Usage: python3 Tests/accuracy_GNB.py CV_NBG_With_LEN
+  Usage: python3 Tests/accuracy_CV.py CV_NBG_With_LEN
 '''
 
 from matplotlib import font_manager
@@ -14,49 +14,49 @@ TotalSamples = 0;
 
 folder = sys.argv[1]
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Virus_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Virus_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
-  if prediction.rstrip('\n') == 'Virus':
+  if prediction.rstrip('\n') == 'Virus' or prediction.rstrip('\n') == 'Viral':
     Hits = Hits + 1;
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Bacteria_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Bacteria_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
   if prediction.rstrip('\n') == 'Bacteria':
     Hits = Hits + 1;
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Archaea_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Archaea_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
   if prediction.rstrip('\n') == 'Archaea':
     Hits = Hits + 1;
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Fungi_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Fungi_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
   if prediction.rstrip('\n') == 'Fungi':
     Hits = Hits + 1;
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Plant_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Plant_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
   if prediction.rstrip('\n') == 'Plant':
     Hits = Hits + 1;
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Protozoa_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Protozoa_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
   if prediction.rstrip('\n') == 'Protozoa':
     Hits = Hits + 1;
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Mitochondrial_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Mitochondrial_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
   if prediction.rstrip('\n') == 'Mitochondrial':
     Hits = Hits + 1;
 
-f = open("Tests/Predictions/"+folder+"/Prediction_Plastid_1-3-7_CV.txt", "r")
+f = open("Tests/Predictions/"+folder+"/Prediction_Plastid_CV.txt", "r")
 for prediction in f:
   TotalSamples = TotalSamples + 1;
   if prediction.rstrip('\n') == 'Plastid':
