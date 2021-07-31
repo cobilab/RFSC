f670a68c1efdb7a3f3ced4281a9f1b42d87a3d9f
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2021-08-01 00:00:48 +0100

    Clean al CSV files

---
+++ cleanAll.sh
@@ -0,0 +1,45 @@
+# Clean AC CSV Files
+rm Analysis/AC/Archaea/* &> /dev/null
+rm Analysis/AC/Bacteria/* &> /dev/null
+rm Analysis/AC/Fungi/* &> /dev/null
+rm Analysis/AC/Mitochondrial/* &> /dev/null
+rm Analysis/AC/Plant/* &> /dev/null
+rm Analysis/AC/Plastid/* &> /dev/null
+rm Analysis/AC/Protozoa/* &> /dev/null
+rm Analysis/AC/Virus/* &> /dev/null
+
+# Clean GeCo CSV Files
+rm Analysis/GeCo/Archaea/* &> /dev/null
+rm Analysis/GeCo/Bacteria/* &> /dev/null
+rm Analysis/GeCo/Fungi/* &> /dev/null
+rm Analysis/GeCo/Mitochondrial/* &> /dev/null
+rm Analysis/GeCo/Plant/* &> /dev/null
+rm Analysis/GeCo/Plastid/* &> /dev/null
+rm Analysis/GeCo/Protozoa/* &> /dev/null
+rm Analysis/GeCo/Virus/* &> /dev/null
+
+# Clean GCcontent CSV Files
+rm Analysis/GCcontent/Archaea/* &> /dev/null
+rm Analysis/GCcontent/Bacteria/* &> /dev/null
+rm Analysis/GCcontent/Fungi/* &> /dev/null
+rm Analysis/GCcontent/Mitochondrial/* &> /dev/null
+rm Analysis/GCcontent/Plant/* &> /dev/null
+rm Analysis/GCcontent/Plastid/* &> /dev/null
+rm Analysis/GCcontent/Protozoa/* &> /dev/null
+rm Analysis/GCcontent/Virus/* &> /dev/null
+
+# Clean LengthSeq CSV Files
+rm Analysis/LengthSeq/Archaea/* &> /dev/null
+rm Analysis/LengthSeq/Bacteria/* &> /dev/null
+rm Analysis/LengthSeq/Fungi/* &> /dev/null
+rm Analysis/LengthSeq/Mitochondrial/* &> /dev/null
+rm Analysis/LengthSeq/Plant/* &> /dev/null
+rm Analysis/LengthSeq/Plastid/* &> /dev/null
+rm Analysis/LengthSeq/Protozoa/* &> /dev/null
+rm Analysis/LengthSeq/Virus/* &> /dev/null
+
+# Clean SVM CSV Files
+rm Analysis/SVM/* &> /dev/null
+
+# Clean KNN CSV Files
+rm Analysis/KNN/* &> /dev/null
