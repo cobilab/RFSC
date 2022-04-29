c5a7c33584097c976a7cbddf13fef0780ca235dd
jalmeida <joao.rafael.almeida@ua.pt>
2022-04-29 16:37:24 +0100

    First draft for accuracy

--- SyntheticSequences/Kraken2/computeWithKraken.py
+++ SyntheticSequences/Kraken2/computeWithKraken.py
@@ -1,20 +1,35 @@
 import sys
 from os import listdir, path, makedirs
 from os.path import isfile, join
+from pprint import pprint
 
 dbLocation="./krakenDB"
 synth_sequences="../synth_merged_sequences/"
 
 def compute():
+    right = {}
+    wrong = {}
     for domain in listdir(synth_sequences):
+        right[domain] = 0
+        wrong[domain] = 0
         for file in listdir(join(synth_sequences, domain)):
-            reportName=file.split(".fna")[0]
-            reportName=join("./report/", reportName)
+            fileName=file.split(".fna")[0]
+            reportName=join("./report/", fileName)
             sequence=join(synth_sequences, domain, file)
             os.system(f"kraken2 --db {dbLocation} --report {reportName} --report-minimizer-data --output output.txt {sequence}")
-
-def classify():
-    print("to do")
+            with open(reportName, 'r') as fp:
+                content = fp.read().rstrip().lower()
+                if domain.lower() in content:
+                    right[domain] += 1
+                else:
+                    wrong[domain] += 1
+        pprint(right)
+        pprint(wrong)
+    for domain in listdir(synth_sequences):
+        rights = right[domain]
+        total = right[domain] + wrong[domain]
+        acc = right/total
+        print(f"Accuracy for {domain}: {acc}")
 
 if __name__ == "__main__":
     if "/" in sys.argv[0]:
@@ -22,4 +37,3 @@ if __name__ == "__main__":
     else:
         warnings.filterwarnings(action='ignore', category=DeprecationWarning)
         compute()
-        classify()
